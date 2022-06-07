bd6a1004a27c7304144f02055fe2deed23e64d17
ammar <amardjebabla10@gmail.com>
2022-06-07 23:41:08 +0200

    adding product entity

---
+++ database/migrations/2022_06_07_204906_add_product_id_to_some_tables.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddProductIdToSomeTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('packs', function ($table) {
+            $table->bigInteger('product_id')->unsigned()->index()->nullable();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        
+        });
+        Schema::table('plats', function ($table) {
+            $table->bigInteger('product_id')->unsigned()->index()->nullable();
+            $table->foreign('product_id')->references('id')->on('plats')->onDelete('cascade');
+         
+        });
+        Schema::table('food', function ($table) {
+            $table->bigInteger('product_id')->unsigned()->index()->nullable();
+            $table->foreign('product_id')->references('id')->on('food')->onDelete('cascade');
+        });
+      
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('some_tables', function (Blueprint $table) {
+            //
+        });
+    }
+}
